f10d7378881522f17d6e84f565a3bac70707be5e
DevChef <devchef318@gmail.com>
2022-02-02 23:06:58 +0530

    first commit

---
+++ PlatformService/Data/PrepDb.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using PlatformService.Models;
+using System;
+using System.Linq;
+
+namespace PlatformService.Data
+{
+    public static class PrepDb
+    {
+        public static void InitilizeDefaultData(IApplicationBuilder app, bool isProd)
+        {
+            using (var serviceScope = app.ApplicationServices.CreateScope())
+            {
+                SeedData(serviceScope.ServiceProvider.GetRequiredService<AppDbContext>(), isProd);
+            }
+        }
+
+        private static void SeedData(AppDbContext context,bool isProd)
+        {
+            if (isProd)
+            {
+                Console.WriteLine("<--- Attempting to Apply Migrations --->");
+                try
+                {
+                    context.Database.Migrate();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error while applying migrations {ex.Message}");
+                }
+            }
+            if (!context.Platforms.Any())
+            {
+                Console.WriteLine("--->Seeding Data.");
+
+                context.Platforms.AddRange(
+                    new Platform() { Name = "Dot Net", Publisher = "Microsoft", Cost = "Free" },
+                    new Platform() { Name = "SQL Server Express", Publisher = "Microsoft", Cost = "Free" },
+                    new Platform() { Name = "Kubernetes", Publisher = "Cloud Native Computing Foundation", Cost = "Free" }
+                    );
+
+                context.SaveChanges();
+            }
+            else
+            {
+                Console.WriteLine("--> We already have data");
+            }
+        }
+    }
+}
